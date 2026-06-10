846375f01fb02e4bb19143e619dae8387b68ed96
Federico Hanselmann <fedehanselmann@MacBook-Air-de-Federico.local>
2026-06-10 17:31:53 -0300

    Add a score and a reset button

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,9 @@
+const score = {
+    wins : 0,
+    losses : 0,
+    ties : 0
+}
+
 function pickComputerMove() {
     const randomNumber = Math.random()
     let computerMove = ''
@@ -20,36 +26,61 @@ function RpsResult(playerMove){
     const computerMove = pickComputerMove()
 
     if (playerMove === 'Rock'){
-        if (computerMove == 'Rock'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , its a TIE!`);
+        if (computerMove === 'Rock'){
+            score.ties += 1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , its a TIE!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
-        else if (computerMove == 'Scissors'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , you won!`);
+        else if (computerMove === 'Scissors'){
+            score.wins += 1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , you won!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
-        else if (computerMove == 'Paper'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , you lost!`);
+        else if (computerMove === 'Paper'){
+            score.losses +=1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , you lost!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
     }
     else if (playerMove === 'Paper'){
-        if (computerMove == 'Paper'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , its a TIE!`);
+        if (computerMove === 'Paper'){
+            score.ties += 1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , its a TIE!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
-        else if (computerMove == 'Rock'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , you won!`);
+        else if (computerMove === 'Rock'){
+            score.wins += 1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , you won!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
-        else if (computerMove == 'Scissors'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , you lost!`);
+        else if (computerMove === 'Scissors'){
+            score.losses +=1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , you lost!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
     }
     else if (playerMove === 'Scissors'){
-        if (computerMove == 'Scissors'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , its a TIE!`);
+        if (computerMove === 'Scissors'){
+            score.ties += 1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , its a TIE!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
-        else if (computerMove == 'Paper'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , you won!`);
+        else if (computerMove === 'Paper'){
+            score.wins +=1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , you won!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
-        else if (computerMove == 'Rock'){
-            alert(`You picked ${playerMove} and the oponent ${computerMove} , you lost!`);
+        else if (computerMove === 'Rock'){
+            score.losses +=1
+            alert(`You picked ${playerMove} and the oponent ${computerMove} , you lost!
+                The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties} `);
         }
     }
+}
+
+function resetScore(){
+    score.wins = 0
+    score.ties = 0
+    score.losses = 0
+    alert(`The score is : Wins = ${score.wins}, Losses = ${score.losses}, Ties : ${score.ties}`)
 }
